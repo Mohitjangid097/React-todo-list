37069648a50dafdfb81ba4e4a688f12c4885cd42
mohit jangid <mohitjangid183@gmail.com>
2021-09-25 00:22:04 +0530

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Footer = () => {
+
+    return (
+        <footer className="bg-dark text-light py-3 " >
+            <p className="text-center">
+                Mohit Jangid  &copy; 2021 All Rights Reserved
+            </p>
+        </footer>
+    )
+}
+
